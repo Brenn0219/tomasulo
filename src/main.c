71cefe246b6290e3bb7600e4d1b298ea972b1323
Brenn0219 <brennoaugusto2109@gmail.com>
2024-05-19 14:49:08 -0300

    test: reordering buffer add

--- src/main.c
+++ src/main.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "reordering.h"
 #include "print.h"
-
-#define INPUT_SIZE 15
 
 int main() {
     const char *path = "data/in.txt";
     FILE *file = fopen(path, "r");
-    char str[INPUT_SIZE]; 
+    Buffer *rb = (Buffer *) malloc(sizeof(Buffer) * TOTAL_BUFFER);
 
     if (file == NULL) {
         perror("error\n");
         return -1;
     }
 
-    Instr instr;
-
-    while (fscanf(file, "%s", str) != EOF) {
-       parse_instr(str, &instr);
-       print_instr(&instr);
-    }
+    reobuf_add(rb, file);
 
     return 0;
 }
