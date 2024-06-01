7057724a6cdf1ed74e8e131593dcd8cafe8251ac
Brenn0219 <brennoaugusto2109@gmail.com>
2024-06-01 16:31:07 -0300

    feat: instruction

---
+++ pkg/instruction/instruction.go
@@ -0,0 +1,24 @@
+package instruction
+
+import (
+	"tomasulo/pkg/registrar"
+)
+
+type InstructionType uint8
+
+const (
+	ADD InstructionType = iota
+	SUB
+	MULT
+	DIV
+	LDR
+	STD
+)
+
+type Instruction struct {
+	Id      uint
+	Type    InstructionType
+	Destiny registrar.Registrar
+	Op1     registrar.Registrar
+	Op2     registrar.Registrar
+}
